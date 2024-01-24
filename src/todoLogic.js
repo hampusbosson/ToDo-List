25031ff755064b037fa6fd0afa3b29825e4e1dce
Hampus Bosson <hampus.bosson@outlook.com>
2024-01-24 18:52:56 +0100

    Add todolist class to handle storing and displaying several todos on the page

--- src/todoLogic.js
+++ src/todoLogic.js
@@ -1,4 +1,4 @@
-class todo {
+class Todo {
     constructor(title, details, date, priority) {
         this.title = title; 
         this.details = details; 
@@ -7,7 +7,30 @@ class todo {
     }
 }
 
-class project {
+class TodoList {
+    constructor() {
+        this.todos = []; 
+    }
+
+    addTodo(todo) {
+        if (!(todo instanceof Todo)) {
+            throw new error("Object mus be an instance of Todo"); 
+        }
+
+        const titleExists = this.todos.some(existingTodo => existingTodo.title === todo.title); 
+        if (titleExists) {
+            throw new error (`A todo with the title "${todo.title}" already exists.`); 
+        }
+
+        this.todos.push(todo); 
+    }
+
+    getTodoByTitle(title) {
+        return this.todos.find(todo => todo.title === title); 
+    }
+}
+
+class Project {
     constructor(title) {
         this.title = title, 
         this.todos = []; 
@@ -26,11 +49,13 @@ class project {
     }
 }
 
-class note {
+class Note {
     constructor(title, details) {
         this.title = title; 
         this.details = details; 
     }
 }
 
-export { todo, project, note }; 
+
+
+export { Todo, Project, Note, TodoList }; 
