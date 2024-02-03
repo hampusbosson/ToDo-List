69cf2d36fedd257bbea26970148ff0766cbb6e23
Hampus Bosson <hampus.bosson@outlook.com>
2024-02-03 14:28:01 +0100

    Add createValueTextarea function to UIhelper module, that creates a textarea that you can assign a value upon creation

--- src/UIhelper.js
+++ src/UIhelper.js
@@ -20,9 +20,34 @@ function createValueInputElement(type, id, name, value, classList) {
     if (value) { // Only set placeholder if a value is provided
         input.value = value;
     }
-    
+
     return input; 
 }
+
+function createValueTextarea(id, name, value, classList) {
+    const textarea = document.createElement('textarea');
+    textarea.id = id;
+    textarea.name = name;
+    textarea.classList.add(classList); // Use spread syntax for multiple classes
+    textarea.wrap = "hard";
+
+    if (value) {
+        textarea.value = value;
+    }
+
+
+    // Event listener to grow the textarea without scrollbar
+    textarea.addEventListener('input', function() {
+        this.style.height = '1px'; // Reset height
+        this.style.height = this.scrollHeight + 'px'; // Set new height based on content
+    });
+
+
+
+    return textarea;
+}
+
+
 
 function hideAllPages() {
     const pages = document.querySelectorAll('.page'); 
@@ -56,4 +81,4 @@ function createIconButton(type, classList, onClick) {
 
 
 
-export { createInputElement, hideAllPages, createButton, hideAllHomePages, createIconButton, createValueInputElement }; 
+export { createInputElement, hideAllPages, createButton, hideAllHomePages, createIconButton, createValueInputElement, createValueTextarea }; 
